4eedf196ec03e49c58c9428e1f0617b116e5e750
F3MDEV <f3m_github@outlook.com>
2022-04-26 15:26:05 +0100

    func: addon Viewport & Screen Reaader

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -6,7 +6,9 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport',
+    'addon-screen-reader'
   ],
   features: {
     postcss: false,
